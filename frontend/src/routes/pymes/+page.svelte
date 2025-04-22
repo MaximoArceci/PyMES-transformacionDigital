2b1ae1a478420df071410ddcd4b64b074b3cd8df
Maximo Arceci <marceci@netnix.net>
2025-04-22 20:13:43 -0300

    cambios esteticos

--- frontend/src/routes/pymes/+page.svelte
+++ frontend/src/routes/pymes/+page.svelte
@@ -113,7 +113,7 @@
 	});
 </script>
 
-<div class="bg-gray-300 min-h-screen p-4">
+<div class="bg-slate-900 min-h-screen p-4">
 <div class="flex flex-col lg:flex-row lg:space-x-6 m-4 ">
 	<!-- Filtros -->
 	<div class="w-full lg:w-1/5 space-y-4">
@@ -125,11 +125,11 @@
 		</button>
 
 		<div>
-			<label for="trabajoRealizado" class="block text-gray-700 text-sm font-medium mb-2">Filtrar por trabajo realizado:</label>
+			<label for="trabajoRealizado" class="block text-sm font-medium mb-2 text-white">Filtrar por trabajo realizado:</label>
 			<select
 				id="trabajoRealizado"
 				bind:value={trabajoRealizadoFiltro}
-				class="w-full p-3 border border-gray-300 rounded-md bg-gray-200"
+				class="w-full p-3 border border-white rounded-md bg-slate-900 text-white"
 			>
 				<option value={null}>Mostrar todos</option>
 				<option value="Sustentabilidad">Sustentabilidad</option>
@@ -139,11 +139,11 @@
 		</div>
 
 		<div>
-			<label for="tipoEmpresa" class="block text-gray-700 text-sm font-medium mb-2">Filtrar por tipo de empresa:</label>
+			<label for="tipoEmpresa" class="block text-sm font-medium mb-2 text-white">Filtrar por tipo de empresa:</label>
 			<select
 				id="tipoEmpresa"
 				bind:value={tipoEmpresaFiltro}
-				class="w-full p-3 border border-gray-300 rounded-md bg-gray-200"
+				class="w-full p-3 border border-white rounded-md bg-slate-900 text-white"
 			>
 				<option value={null}>Mostrar todos</option>
 				<option value="Micro-Pyme">Micro-Pyme</option>
@@ -153,11 +153,11 @@
 		</div>
 
 		<div>
-			<label for="sector" class="block text-gray-700 text-sm font-medium mb-2">Filtrar por Sector:</label>
+			<label for="sector" class="block text-sm font-medium mb-2 text-white">Filtrar por Sector:</label>
 			<select
 				id="sector"
 				bind:value={sectorFiltro}
-				class="w-full p-3 border border-gray-300 rounded-md bg-gray-200"
+				class="w-full p-3 border border-white rounded-md bg-slate-900 text-white"
 			>
 				<option value={null}>Mostrar todos</option>
 				<option value="Servicios">Servicios</option>
@@ -171,7 +171,7 @@
 	</div>
 
 	<!-- Mapa -->
-	<div class="w-full lg:w-4/5 h-[950px] border border-gray-300 rounded-lg overflow-hidden shadow-md mt-6 lg:mt-0">
+	<div class="w-full lg:w-4/5 h-[900px] border border-gray-300 rounded-lg overflow-hidden shadow-md mt-6 lg:mt-0">
 		<div id="map" class="w-full h-full rounded-lg"></div>
 	</div>
 </div>
